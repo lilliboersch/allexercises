bc333664ed1a7ed0d3b47725346bb0160ebf1192
Lilli <boerschlilli5@gmail.com>
2024-01-17 18:22:41 +0100

    Session 3-4 Practice

--- session34.py
+++ session34.py
@@ -1,2 +1,10 @@
 print("hello world again!")
 print("hello world again! this is a new change")
+
+True #Boolean value
+False #Bolean value
+None #No available data
+
+print(5/5) #always make sure to keep .0 at the end (expressed as floating number)
+
+print(10//3)
